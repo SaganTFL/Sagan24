3483ca508b26b99b2df16cad21d00d3f1b54bb63
Weyaxi <81961593+Weyaxi@users.noreply.github.com>
2024-09-16 11:04:38 +0300

    otonom pro plus xs

--- Autonom.py
+++ Autonom.py
@@ -1,19 +1,16 @@
 import cv2
 import numpy as np
+from video import Video
 from MainSystem import AUVController
 
 controller = AUVController('udpin:0.0.0.0:14550')
-print("arm start")
+print("Arm Start")
 controller.arm_vehicle()
-print("arm success")
+print("Arm Success")
 
+# Flags
+big = 0
 seen = 0
-was_big = 0
-
-#!/usr/bin/env python
-"""
-BlueRov video capture class
-"""
 
 def kordinat_to_yazi(coordinates, frame_width, frame_height):
     if coordinates == [None]: return None
@@ -77,196 +74,81 @@ def detect_red_color(frame):
     if contours:
         largest_contour = max(contours, key=cv2.contourArea)
         x, y, w, h = cv2.boundingRect(largest_contour)
-        return (x, y, w, h)
+        if(w*h>200):
+            return (x, y, w, h)
+        return None
 
     # Kırmızı alan bulunamazsa None döndür
     return None
+
 
 def show_image(img):
     cv2.imshow("Image Window", img)
     cv2.waitKey(3)
 
-def image_callback(cv_image: cv2.Mat, controller : AUVController):
-    global was_big, seen
+
+def image_callback(cv_image: cv2.Mat, controller: AUVController):
+    global big, seen
+
     show_image(cv_image)
+    
     detection_res = detect_red_color(cv_image)
-    if(detection_res==None):
-        if(seen==1):
 
-            seen = 2
-        if(seen==2):
-            print("ileri")
-            controller.go_to_vehicle(1, 0, 0, 0)
-        else:
-            print("durdu")
-            controller.go_to_vehicle(0,0,0,0)
-        print("No flag")
-    elif((detection_res[2]*detection_res[3]>5000 or was_big ==1) and seen != 2):
-        print("aşağı")
+    if detection_res and detection_res[2] * detection_res[3] > 10000: # if the color area too big it is big
+        big = 1
+        print("Big flag true")
+    elif not detection_res and big: # we already passed through the door, so we now not see the color but it it already seen, so make seen flag true
+        big = 0
+        seen = 1
+        print("Görüldü, seen flag true")
+
+    # main movement
+
+    if not detection_res and not seen:
+        print("Bulunamadı - Araç dönüyor")
+        controller.go_to_vehicle(0,0,0,0.3) 
+       
+    elif big:
+        print("Aşağı")
         controller.go_to_vehicle(0,0,-1,0)
+  
+    elif seen:
+        print("İleri")
+        controller.go_to_vehicle(1,0,0,0)
 
-        was_big = 1
     else:
-        if(seen==0):
-
-            seen = 1
         res = kordinat_to_yazi(detection_res, cv_image.shape[1],cv_image.shape[0])
         print(res)
-        print("sağ")
-        controller.go_to_vehicle(0,-1,0,0)
-        if(res[0][0]=="right"):
-            print("sağ")
-            controller.go_to_vehicle(0,1,0,0)
-        elif(res[0][0]=="left"):
-            print("sol")
-            controller.go_to_vehicle(0,1,0,0)
-            print("moved")
-        elif(res[0][1]=="bottom" or res[0][1]=="middle"):
-            print("aşağı")
+
+        usey = 0 # vey far away from object
+        if detection_res[2] * detection_res[3] < 500:
+            usey = 1
+
+
+        if res[0][0] == "right":
+            print("Sağ")
+            if usey:
+                controller.go_to_vehicle(0.3,-0.5,0,0)
+            else:
+                controller.go_to_vehicle(0,0,0,-0.5)
+
+        elif res[0][0] == "left":
+            print("Sol")
+            if usey:
+                controller.go_to_vehicle(0.3,0.5,0,0)
+            else:
+                controller.go_to_vehicle(0,0,0,0.5)
+            
+        elif res[0][1] == "bottom" or res[0][1] == "middle":
+            print("Aşağı")
             controller.go_to_vehicle(0,0,-1,0)
-        else :
-            print("durdu")
-            controller.go_to_vehicle(0,0,0,0)
-        if(res[0][0]=="middle" and res[0][1]=="top"):
-            print("ileri")
+
+        else:
+            print("İleri")
             controller.go_to_vehicle(1,0,0,0)
 
 
-
-
-class Video():
-    """BlueRov video capture class constructor
-
-    Attributes:
-        port (int): Video UDP port
-        video_codec (string): Source h264 parser
-        video_decode (string): Transform YUV (12bits) to BGR (24bits)
-        video_pipe (object): GStreamer top-level pipeline
-        video_sink (object): Gstreamer sink element
-        video_sink_conf (string): Sink configuration
-        video_source (string): Udp source ip and port
-        latest_frame (np.ndarray): Latest retrieved video frame
-    """
-
-    def __init__(self, port=5600):
-        """Summary
-
-        Args:
-            port (int, optional): UDP port
-        """
-
-        Gst.init(None)
-
-        self.port = port
-        self.latest_frame = self._new_frame = None
-
-        # [Software component diagram](https://www.ardusub.com/software/components.html)
-        # UDP video stream (:5600)
-        self.video_source = 'udpsrc port={}'.format(self.port)
-        # [Rasp raw image](http://picamera.readthedocs.io/en/release-0.7/recipes2.html#raw-image-capture-yuv-format)
-        # Cam -> CSI-2 -> H264 Raw (YUV 4-4-4 (12bits) I420)
-        self.video_codec = '! application/x-rtp, payload=96 ! rtph264depay ! h264parse ! avdec_h264'
-        # Python don't have nibble, convert YUV nibbles (4-4-4) to OpenCV standard BGR bytes (8-8-8)
-        self.video_decode = \
-            '! decodebin ! videoconvert ! video/x-raw,format=(string)BGR ! videoconvert'
-        # Create a sink to get data
-        self.video_sink_conf = \
-            '! appsink emit-signals=true sync=false max-buffers=2 drop=true'
-
-        self.video_pipe = None
-        self.video_sink = None
-
-        self.run()
-
-    def start_gst(self, config=None):
-        """ Start gstreamer pipeline and sink
-        Pipeline description list e.g:
-            [
-                'videotestsrc ! decodebin', \
-                '! videoconvert ! video/x-raw,format=(string)BGR ! videoconvert',
-                '! appsink'
-            ]
-
-        Args:
-            config (list, optional): Gstreamer pileline description list
-        """
-
-        if not config:
-            config = \
-                [
-                    'videotestsrc ! decodebin',
-                    '! videoconvert ! video/x-raw,format=(string)BGR ! videoconvert',
-                    '! appsink'
-                ]
-
-        command = ' '.join(config)
-        self.video_pipe = Gst.parse_launch(command)
-        self.video_pipe.set_state(Gst.State.PLAYING)
-        self.video_sink = self.video_pipe.get_by_name('appsink0')
-
-    @staticmethod
-    def gst_to_opencv(sample):
-        """Transform byte array into np array
-
-        Args:
-            sample (TYPE): Description
-
-        Returns:
-            TYPE: Description
-        """
-        buf = sample.get_buffer()
-        caps_structure = sample.get_caps().get_structure(0)
-        array = np.ndarray(
-            (
-                caps_structure.get_value('height'),
-                caps_structure.get_value('width'),
-                3
-            ),
-            buffer=buf.extract_dup(0, buf.get_size()), dtype=np.uint8)
-        return array
-
-    def frame(self):
-        """ Get Frame
-
-        Returns:
-            np.ndarray: latest retrieved image frame
-        """
-        if self.frame_available:
-            self.latest_frame = self._new_frame
-            # reset to indicate latest frame has been 'consumed'
-            self._new_frame = None
-        return self.latest_frame
-
-    def frame_available(self):
-        """Check if a new frame is available
-
-        Returns:
-            bool: true if a new frame is available
-        """
-        return self._new_frame is not None
-
-    def run(self):
-        """ Get frame to update _new_frame
-        """
-
-        self.start_gst(
-            [
-                self.video_source,
-                self.video_codec,
-                self.video_decode,
-                self.video_sink_conf
-            ])
-
-        self.video_sink.connect('new-sample', self.callback)
-
-    def callback(self, sink):
-        sample = sink.emit('pull-sample')
-        self._new_frame = self.gst_to_opencv(sample)
-
-        return Gst.FlowReturn.OK
-
-
-if __name__ == '__main__':
+if __name__ == '__main__': # erkaaammmmmmmm #
     # Create the video object
     # Add port= if is necessary to use a different one
     video = Video()
@@ -302,4 +184,3 @@ if __name__ == '__main__':
         print("test2")
         image_callback(image,controller)
         print("test3")
-
